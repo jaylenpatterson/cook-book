f5243484df88ee8f5b344a60b94c161b8c89360b
Jaylen patterson <jaylenpatterson16@gmail.com>
2022-03-28 22:37:44 -0400

    refactored event emitters by using subjects

--- cook-book/src/app/recipes/recipe.service.ts
+++ cook-book/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
@@ -6,7 +7,6 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.averiecooks.com/wp-content/uploads/2021/01/garlicbutterchicken-5.jpg', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
